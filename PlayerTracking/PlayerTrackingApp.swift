5744efed00c193d99c1e15f0b53e5fca707d93aa
pottsie <pottsma@me.com>
2023-11-27 16:02:21 -0500

    Initial Commit

---
+++ PlayerTracking/PlayerTrackingApp.swift
@@ -0,0 +1,17 @@
+//
+//  PlayerTrackingApp.swift
+//  PlayerTracking
+//
+//  Created by Michael Potts on 11/27/23.
+//
+
+import SwiftUI
+
+@main
+struct PlayerTrackingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
